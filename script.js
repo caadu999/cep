e3f467346fbbd61565d4a73bcbc0d51046de2577
Cadu <cadums013@gmail.com>
2026-03-27 13:14:13 -0300

    Web storage de nome e e-mail

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 const cep = document.getElementById("cep");
+const cepValue = document.getElementById("cep").value;
+const email = document.getElementById("email");
+
+const nome = document.getElementById("seunome");
+
+// WEB STORAGE DO E-MAIL
+email.addEventListener("input", function () {
+  const emailValue = document.getElementById("email").value;
+  sessionStorage.setItem("email", emailValue);
+});
+
+window.addEventListener("load", function () {
+  const emailSalvo = sessionStorage.getItem("email");
+  email.value = emailSalvo;
+});
+
+// WEB STORAGE DO NOME
+
+nome.addEventListener("input", function () {
+  const nomeValue = document.getElementById("seunome").value;
+  sessionStorage.setItem("nome", nomeValue);
+});
+
+window.addEventListener("load", function(){
+  const nomeSalvo = sessionStorage.getItem("nome")
+  nome.value = nomeSalvo
+})
 
 cep.addEventListener("blur", function (event) {
   const elemento = event.target;
