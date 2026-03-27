44a9aef27b0ae3de738737607d3c91a109c5a7d3
Cadu <cadums013@gmail.com>
2026-03-27 15:53:52 -0300

    web storage em todos

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
 const cep = document.getElementById("cep");
 const cepValue = document.getElementById("cep").value;
 const email = document.getElementById("email");
+const endereco = document.getElementById("nome");
+const bairro = document.getElementById("bairro");
+const estado = document.getElementById("estado");
 
 const nome = document.getElementById("seunome");
 
+//WEB STORAGE DO CEP
+
+cep.addEventListener("input", function () {
+  const cepValue = cep.value;
+  sessionStorage.setItem("cep", cepValue);
+});
+
+window.addEventListener("load", function () {
+  const cepSalvo = sessionStorage.getItem("cep");
+  cep.value = cepSalvo;
+});
+
+//WEB STORAGE DO ENDERECO
+
+endereco.addEventListener("input", function () {
+  const eValue = endereco.value;
+  sessionStorage.setItem("end", eValue);
+});
+
+window.addEventListener("load", function () {
+  const eSalvo = sessionStorage.getItem("end");
+  endereco.value = eSalvo;
+});
+
+//WEB STORAGE DO ESTADO
+estado.addEventListener("input", function () {
+  const estadoValue = estado.value;
+  sessionStorage.setItem("estado", estadoValue);
+});
+
+window.addEventListener("load", function () {
+  const estadoSalvo = sessionStorage.getItem("estado");
+  estado.value = estadoSalvo;
+});
+
+// WEB STORAGE BAIRRO
+bairro.addEventListener("input", function () {
+  const bairroValue = bairro.value;
+  sessionStorage.setItem("bairro", bairroValue);
+});
+
+window.addEventListener("load", function () {
+  const bairroSalvo = sessionStorage.getItem("bairro");
+  bairro.value = bairroSalvo;
+});
+
 // WEB STORAGE DO E-MAIL
 email.addEventListener("input", function () {
-  const emailValue = document.getElementById("email").value;
+  const emailValue = email.value;
   sessionStorage.setItem("email", emailValue);
 });
 
@@ -22,10 +71,10 @@ nome.addEventListener("input", function () {
   sessionStorage.setItem("nome", nomeValue);
 });
 
-window.addEventListener("load", function(){
-  const nomeSalvo = sessionStorage.getItem("nome")
-  nome.value = nomeSalvo
-})
+window.addEventListener("load", function () {
+  const nomeSalvo = sessionStorage.getItem("nome");
+  nome.value = nomeSalvo;
+});
 
 cep.addEventListener("blur", function (event) {
   const elemento = event.target;
